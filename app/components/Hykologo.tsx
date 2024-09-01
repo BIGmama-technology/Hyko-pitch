6b395e93f8e5b25802314a16166869be4cb0ce2b
1hachem <hb@big-mama.io>
2024-09-01 20:31:59 +0100

    feat: add hyko moto

---
+++ app/components/Hykologo.tsx
@@ -0,0 +1,7 @@
+export default function HykoLogo() {
+  return (
+    <div className="absolute top-4 left-4 ml-2 text-blue-600 font-semibold text-xl">
+      For us AI is chocolate 🍫
+    </div>
+  );
+}
