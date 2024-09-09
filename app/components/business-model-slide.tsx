a8fbbc29274e13dd1abc13601ff29ca1b652b580
chiefkarim <krimotop2016@gmail.com>
2024-09-09 13:49:30 +0100

    feat: update business model layout

--- app/components/business-model-slide.tsx
+++ app/components/business-model-slide.tsx
@@ -1,18 +1,18 @@
 export function BusinessModel() {
-  return <div className="flex w-full h-full flex-col items-center gap-y-2 sm:gap-y-8 text-blue-600 justify-center px-4">
+  return <div className="flex w-full h-full flex-col items-center  text-blue-600 justify-center px-4">
     <h2 className="text-2xl sm:text-4xl font-bold  text-center">
       Our Business Model:
     </h2>
     <p className="text-xl sm:text-2xl sm:text-center textblue-500 ">Our Figures </p>
 
-    <div className="flex justify-between w-full">
+    <div className=" grid grid-cols-2  justify-between w-full my-4 sm:my-10">
       <div className="text-center">
         <p className="text-lg font-bold sm:text-2xl sm:text-center textblue-500 ">Pre-revenue valuation:</p>
 
         <p className="text-lg  sm:text-2xl sm:text-center textblue-500 ">€25,000,000.00</p>
       </div>
       <div className="text-center">
-        <p className="text-lg font-bold  sm:text-2xl sm:text-center textblue-500 ">Pre-revenue valuation: </p>
+        <p className="text-lg font-bold  sm:text-2xl sm:text-center textblue-500 ">Our need:</p>
 
         <p className="text-lg  sm:text-2xl sm:text-center textblue-500 ">€2,500,000.00</p>
       </div>
@@ -23,7 +23,7 @@ export function BusinessModel() {
       </h2>
       <p className="text-xl sm:text-2xl text-center  textblue-500 ">(SaaS with two kinds of licenses)</p>
     </div>
-    <div className="grid grid-cols-2   justify-between">
+    <div className="grid grid-cols-2 w-full my-4  sm:my-10   justify-between">
       <div>
         <div className="flex justify-center my-2">
           <img src='/down-left-arrow.svg' width={32} height={32} alt='down left arrow' />
@@ -32,14 +32,14 @@ export function BusinessModel() {
           Do it yourself license:       </h2>
         <p className="text-xl sm:text-2xl text-center  textblue-500 "> €200 per user per month/user</p>
       </div>
-      <div>
+      <div >
         <div className="flex justify-center my-2">
           <img src='/down-right-arrow.svg' width={32} height={32} alt='down right arrow' />
         </div>
-        <h2 className="text-xl sm:text-4xl font-bold   text-center">
+        <h2 className="text-xl sm:text-4xl font-bold  mx-auto  text-center">
           Custom license:
         </h2>
-        <p className="text-xl sm:text-2xl text-center  textblue-500 "> 6 hours per mon﻿th, with a prototype manager for €1,500/month/user</p>
+        <p className="text-xl sm:text-2xl text-center mx-auto textblue-500 max-w-md"> 6 hours per mon﻿th, with a prototype manager for €1,500/month/user</p>
       </div>
     </div>
   </div>
