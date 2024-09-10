29478c02a2dcb3a2a8f17a908f6ec636207c3f89
chiefkarim <krimotop2016@gmail.com>
2024-09-10 15:51:42 +0100

    feat: update team

--- app/components/constants.ts
+++ app/components/constants.ts
@@ -1,79 +1,82 @@
-export interface TeamProps {
+export interface MemberProps {
   tagline?: string;
   role: string;
   fullname: string;
   image: string;
-  linkedin: string;
 }
 
-export const teamContent: TeamProps[] = [
+export const teamContent: MemberProps[] = [
   {
     fullname: "Hadj Khelil",
     role: "CEO and founder of BIGmama",
-    linkedin: "https://www.linkedin.com/in/hadj-khelil-60b0952/",
     image: "/team/hadj.png",
   },
   {
     fullname: "Hachem Betrouni",
     role: "CTO",
-    linkedin: "https://dz.linkedin.com/in/hachem-betrouni",
     image: "/team/hachem.png",
   },
   {
     fullname: "Ouassim Abdelmalek GHRIBI",
     role: "Lead Software Engineer",
-    linkedin: "https://dz.linkedin.com/in/ouassimg",
     image: "/team/ouassim.png",
   },
   {
     fullname: "Samir Cheraft",
     role: "Office Manager",
-    linkedin: "https://dz.linkedin.com/in/samir-cheraft-94449a57",
     image: "/team/samir.png",
   },
   {
     fullname: "Anfal Yousra BOUCETTA",
     role: "Software Engineer",
-    linkedin: "https://www.linkedin.com/in/anfal-yousra-boucetta/",
     image: "/team/yousra.png",
   },
   {
     fullname: "Louai Boumediene",
     role: "Backend Developer",
-    linkedin: "https://www.linkedin.com/in/louai-boumediene-018919262/",
     image: "/team/louai.png",
   },
   {
     fullname: "Abdelkarim menna",
     role: "Frontend Developer",
-    linkedin: "https://www.linkedin.com/in/karimmenna/",
     image: "/team/karim.png",
   },
   {
     fullname: "Darine Tag",
     role: "Product Designer ",
-    linkedin: "https://www.linkedin.com/in/darine-tag/",
     image: "/team/darine.JPG",
   },
   {
     fullname: "Abderrahmane Hadj-Nacer",
     role: "Strategic Advisor",
-    linkedin: "https://fr.wikipedia.org/wiki/Abderrahmane_Hadj-Nacer",
     image: "/team/3ami-nacer.png",
-  },
-  {
-    fullname: "Amira Belkis BOUSSOUF",
-    role: "Legal Advisor",
-    linkedin: "https://www.linkedin.com/in/amira-belkis-boussouf-a6943bba/",
-    image: "/team/amira.png",
   },
   {
     fullname: "Mahdi KALLEL",
     role: "Scientific Advisor",
-    linkedin: "https://fr.linkedin.com/in/kallel-mahdi",
     image: "/team/mahdi.png",
   },
+
 ];
+
+export const boardMembers: MemberProps[] = [
+
+  {
+    fullname: "Jacques VINCENT",
+    role: "Share holder",
+    image: "/team/jaques-vincent.png",
+  },
+  {
+    fullname: "J-P HERTEMAN",
+    role: "Share holder",
+    image: "/team/j-p-herteman.png",
+  },
+  {
+    fullname: "Edgar MORIN",
+    role: "Share holder",
+    image: "/team/edgar-morin.png",
+  }
+]
 
 export interface FaqProps {
   title: string;
