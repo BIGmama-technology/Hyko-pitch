1a40a881b19499807e32bdaa32964cfe6332e622
chiefkarim <krimotop2016@gmail.com>
2024-09-10 13:12:34 +0100

    feat: update content

--- app/components/slide-1.tsx
+++ app/components/slide-1.tsx
@@ -2,10 +2,10 @@ export default function OpeningSlide() {
   return (
     <div className="flex w-full h-full flex-col items-center text-center gap-y-2 sm:gap-y-8 text-blue-600 justify-center px-4">
       <h2 className="text-2xl sm:text-4xl font-bold  mb-8 ">
-        We have been building custom AI applications as craftsmen for 10 years
+        We have been building custom AI agents as craftsmen for 10 years
       </h2>
       <p className="text-xl sm:text-2xl sm:text-center">
-        Kwanko - Arkia - Ratp - Total - Sonatrach - Ooredoo - Yourscrib
+        Kwanko - Arkia - Ratp - Total - Sonatrach - Ooredoo - Yourscrib and many more
       </p>
       <p className="text-xl sm:text-2xl  mt-2 sm:mt-10 sm:text-center">
         We decided in 2022 to move from consulting to SaaS: allowing our users
