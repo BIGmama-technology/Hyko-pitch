fff0f4d68b3b74cabcc3a2391920595c3588c389
chiefkarim <krimotop2016@gmail.com>
2024-09-10 10:14:46 +0100

    feat: faq changes

--- app/components/faq-slide.tsx
+++ app/components/faq-slide.tsx
@@ -5,12 +5,12 @@ import { faqContent } from "./constants";
 
 export function FaqSlide() {
   return <div className="text-blue-600  max-h-[75vh] sm:max-h-[80vh] mx-4 h-full text-center flex flex-col justify-center">
-    <h2 className="text-2xl sm:text-2xl font-bold mb-8">What they usually ask ?</h2>
+    <h2 className="text-2xl sm:text-4xl font-bold mb-8">What they usually ask ?</h2>
     <Accordion type="multiple" className="overflow-y-scroll">
       {faqContent.map(faq => (
         <AccordionItem value={faq.title} key={faq.title} className='hover:bg-muted rounded-md  sm:p-6 transition-all'>
-          <AccordionTrigger className=' hover:no-underline  '>
-            <h2 className='text-lg font-bold md:text-2xl w-full'>{faq.title}</h2>
+          <AccordionTrigger className=' '>
+            <h2 className='text-lg font-semibold md:text-2xl w-full'>{faq.title}</h2>
           </AccordionTrigger>
           <AccordionContent>
             <ul >
