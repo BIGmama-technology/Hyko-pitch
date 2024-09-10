0a379fb09ee66754d477af98b6e65beb8c2a972e
chiefkarim <krimotop2016@gmail.com>
2024-09-10 10:35:57 +0100

    fix: videos overflow on mobile

--- app/components/clients-slide.tsx
+++ app/components/clients-slide.tsx
@@ -7,13 +7,13 @@ export function ClientsSlide() {
     "e2OtBpoe2kE",
   ];
   return (
-    <div className="flex w-full flex-col items-center justify-center text-blue-600 max-h-[75vh] sm:max-h-[80vh]  px-4 overflow-y-scroll sm:overflow-y-hidden ">
+    <div className="flex w-full flex-col items-center justify-center text-blue-600 max-h-[75vh] sm:max-h-[80vh]  px-4 ">
       <h2 className="text-2xl sm:text-4xl font-bold text-center mb-8">Our clients are Happy !</h2>
-      <div className="grid justify-center justify-items-center items-center lg:grid-cols-3 md:grid-cols-2  grid-col-1 gap-3 ">
+      <div className="grid justify-center justify-items-center items-center lg:grid-cols-3 md:grid-cols-2  grid-col-1 gap-3 overflow-y-scroll md:overflow-y-hidden ">
         {videoIds.map((id) => (
           <iframe
             key={id}
-            className="min-h-64 aspect-[16_/_9]"
+            className="min-h-52 sm:min-h-64 "
             src={`https://www.youtube.com/embed/${id}`}
             allowFullScreen
           ></iframe>
