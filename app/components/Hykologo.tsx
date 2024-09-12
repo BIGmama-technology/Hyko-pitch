57e06d198de0dae1facb2930b2bdbefd3bbc9495
chiefkarim <krimotop2016@gmail.com>
2024-09-12 12:43:28 +0100

    feat: fix unvisible navigation on viewport hieght 600

--- app/components/Hykologo.tsx
+++ app/components/Hykologo.tsx
@@ -1,6 +1,6 @@
 export default function HykoLogo() {
   return (
-    <div className="my-8 px-6 flex justify-center sm:justify-start  min-w-full  text-blue-600 font-semibold text-xl">
+    <div className="my-2 px-6 flex justify-center sm:justify-start  min-w-full  text-blue-600 font-semibold text-xl">
       For us AI is chocolate 🍫
     </div>
   );
