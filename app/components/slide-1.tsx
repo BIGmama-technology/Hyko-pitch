e3bd2d4c8b02543c95a519f239ae3ff5549d395e
1hachem <hb@big-mama.io>
2024-10-08 09:43:24 +0100

    fix: typo and sentence formulation

--- app/components/slide-1.tsx
+++ app/components/slide-1.tsx
@@ -1,16 +1,21 @@
 export default function OpeningSlide() {
-  return (
-    <div className="flex w-full h-full flex-col items-center text-center gap-y-2 sm:gap-y-8 text-blue-600 justify-center px-4">
-      <h2 className="text-2xl sm:text-3xl  mb-8 ">
-        We have been building custom AI agents as craftsmen for 10 years:
-      </h2>
-      <p className="text-xl sm:text-2xl sm:text-center">
-        Kwanko - Arkia - Ratp - Total - Sonatrach - Ooredoo - Yourscrib and many more
-      </p>
-      <p className="text-2xl sm:text-4xl  mt-2 sm:mt-10 font-semibold sm:text-center">
-        We decided in 2022 to move from consulting to SaaS: allowing our users
-        to build AI agents by themselves using Hyko!
-      </p>
-    </div>
-  );
+	return (
+		<div className="flex w-full h-full flex-col items-center text-center gap-y-2 sm:gap-y-8 text-blue-600 justify-center px-4">
+			<h2 className="text-2xl sm:text-3xl  mb-8 ">
+				We have been building custom AI agents as craftsmen for 10 years:
+			</h2>
+			<p className="text-xl sm:text-2xl sm:text-center">
+				Kwanko - Arkea - Ratp - Total - Sonatrach - Ooredoo - Yourscrib and many
+				more
+			</p>
+			<p className="text-2xl sm:text-4xl  mt-2 sm:mt-10 font-semibold sm:text-center">
+				We decided in 2022 to move from consulting to SaaS.
+			</p>
+
+			<p className="text-2xl sm:text-4xl  mt-2 sm:mt-10 font-semibold sm:text-center">
+				The problem we sloved is allowing non-technical users to build AI agents
+				by themselves !
+			</p>
+		</div>
+	);
 }
