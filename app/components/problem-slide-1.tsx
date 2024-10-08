c54c568d01cd644d248fc1fc943f3934c0942e2c
1hachem <hb@big-mama.io>
2024-10-08 09:43:41 +0100

    fix: layout and added mckeinsey link

--- app/components/problem-slide-1.tsx
+++ app/components/problem-slide-1.tsx
@@ -1,16 +1,30 @@
 export function ProblemSlide1() {
-  return (<div className="flex w-full h-full flex-col items-center justify-center  text-center text-blue-600 px-4 ">
-    <h2 className=" text-2xl sm:text-4xl font-bold mb-8">
-      What did we learn from 10 years in the ground field ?     </h2>
-    <p className="font-medium text-3xl "> Agents are the future of AI: why?</p>
-    <p className=" text-xl md:text-2xl py-2 ">Because agents offer a solution to the biggest AI issues, Agents make AI:</p>
-    <ul className="text-xl text-start md:text-2xl space-y-1 max-w-6xl mx-auto list-disc">
-      <li>Affordable</li>
-      <li>Explainable</li>
-      <li>Feasible</li>
-      <li>Sharable</li>
-      <li>Useful</li>
-    </ul>
-  </div>
-  )
+	return (
+		<div className="flex w-full h-full flex-col items-center justify-center text-center text-blue-600 px-4  mx-auto">
+			<h2 className="text-2xl sm:text-4xl font-bold mb-6">
+				What did we learn from 10 years in the ground field?
+			</h2>
+			<p className="font-medium text-2xl mb-4">
+				Agents are the future of AI: why?
+			</p>
+			<p className="text-lg md:text-xl mb-4">
+				Because agents offer a solution to the biggest AI issues, Agents make
+				AI:
+			</p>
+			<ul className="text-lg md:text-xl space-y-2 list-disc text-left mb-6">
+				<li>Affordable</li>
+				<li>Explainable</li>
+				<li>Feasible</li>
+				<li>Sharable</li>
+				<li>Useful</li>
+			</ul>
+
+			<a
+				className="text-lg md:text-xl underline"
+				href="https://www.mckinsey.com/capabilities/mckinsey-digital/our-insights/why-agents-are-the-next-frontier-of-generative-ai?stcr=AF6B9CB453764286AF46902E388F0540&cid=other-eml-alt-mip-mck&hlkid=7fd5040f72544d80b7ed7a367130646e&hctky=14813738&hdpid=62a61fa0-2d7d-4f20-997f-b7375d5d1f4c"
+			>
+				According to McKinsey's insights
+			</a>
+		</div>
+	);
 }
